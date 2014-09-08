9d4ff827fcdadac3a42d12ff129d32858135b6b2
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-09-08 18:22:14 -0300

    Initial Commit

---
+++ RoboFace/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RoboFace
+//
+//  Created by Marcelo Sampaio on 9/8/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RoboFace/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RoboFace
+//
+//  Created by Marcelo Sampaio on 9/8/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
